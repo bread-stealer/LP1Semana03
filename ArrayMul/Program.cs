4bbef2f781a32f183f5e607981d1233cf536e8ec
bread-stealer <guilhermesilvanegrinho@gmail.com>
2026-03-04 12:58:55 +0000

    Added ArrayMul to the solution

---
+++ ArrayMul/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayMul
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
