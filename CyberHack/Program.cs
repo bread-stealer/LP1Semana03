4f46c18ae347449d72359df61f819694dac35a04
bread-stealer <guilhermesilvanegrinho@gmail.com>
2026-03-05 16:41:22 +0000

    Added foreach for enums

--- CyberHack/Program.cs
+++ CyberHack/Program.cs
@@ -1,12 +1,70 @@
 ﻿using System;
 
+enum HackTool
+{
+    BruteForce,
+    Phishing,
+    Backdoor,
+    ZeroDay,
+    AIOverride,
+    QuantumExploit
+}
+
+enum SystemType
+{
+    CorporateServer,
+    BankDatabase,
+    SmartCityCore,
+    MilitaryAI
+}
+
 namespace CyberHack
 {
     public class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            // Create the following enums
+            // HackTool > BruteForce, Phishing, Backdoor, ZeroDay, AIOverride, QuantumExploit
+            // SystemType > CorporateServer, BankDatabase, SmartCityCore, MilitaryAI
+
+            // The principal program should ask for the user type of system to attack and one or more
+            // tools of hacking. Convert the value inserted in respective enum types
+
+            // Implement the necessary logic to determine if the system was hacked, according to the following conditions:
+            // CorporateSever > (Phishing or Backdoor) and (BruteForce or ZeroDay)
+            // BankDatabase > (ZeroDay and Backdoor) or (QuantumExploit and AiOverride)
+            // SmartCityCore > (AIOverride and Backdoor) or (ZeroDay and BruteForce) or (QuantumExploit and Phishing)
+            // MilitaryAI > (ZeroDay and AIOverride) and (Backdoor or BruteForce) and (Phishing or QuantumExploit)
+
+            // The program should appear a final message indicating that the system was hacked with success
+            // ("System Hacked") or if the attack failed ("System Secure")
+
+            string systemInput = Console.ReadLine();
+            SystemType system = (SystemType)Enum.Parse(typeof(SystemType), systemInput);
+
+            string toolsInput = Console.ReadLine();
+            string[] toolNames = toolsInput.Split(' ');
+
+            bool bruteForce = false, phishing = false, backdoor = false;
+            bool zeroDay = false, aiOverride = false, quantumExploit = false;
+
+            foreach (string toolName in toolNames)
+            {
+                HackTool tool = (HackTool)Enum.Parse(typeof(HackTool), toolName);
+                switch (tool)
+                {
+                    case HackTool.BruteForce:    bruteForce = true;    break;
+                    case HackTool.Phishing:      phishing = true;      break;
+                    case HackTool.Backdoor:      backdoor = true;      break;
+                    case HackTool.ZeroDay:       zeroDay = true;       break;
+                    case HackTool.AIOverride:    aiOverride = true;    break;
+                    case HackTool.QuantumExploit:quantumExploit = true; break;
+                }
+            }
+
+
+            
         }
     }
 }
