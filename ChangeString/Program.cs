83ee5e7b7115b22413df35687b32ae58d729a529
bread-stealer <guilhermesilvanegrinho@gmail.com>
2026-03-05 15:23:49 +0000

    Added resolution for ChangeString

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,7 +6,32 @@ namespace ChangeString
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            //Create a program that:
+            // Asks the user for a string
+            // Asks the user for a character
+            // Prints the string replacing the character with x
+            // 3 Example:
+            // String: How are you?
+            // Character: o
+            // Will be printed: Hxw are yxu?
+
+            Console.Write("String: ");
+            string inputString = Console.ReadLine();
+
+            Console.Write("Character: ");
+            char inputChar = Console.ReadLine()[0];
+
+            string outputString = "";
+            foreach (char c in inputString)
+            {
+                if (c == inputChar)
+                    outputString += 'x';
+
+                else
+                    outputString += c;
+            }
+
+            Console.WriteLine(outputString);
         }
     }
 }
